f03c908351524b8f9467b848af1c2798125f3674
kazuhito-m <kazuhito.sumpic@gmail.com>
2016-11-16 18:01:29 +0900

    機能追加。Redisのサンプル書くエントリポイント追加。

--- src/main/scala/com/github/kazuhito_m/twitterbattler/primitive/view/SampleController.scala
+++ src/main/scala/com/github/kazuhito_m/twitterbattler/primitive/view/SampleController.scala
@@ -3,8 +3,13 @@ package com.github.kazuhito_m.twitterbattler.primitive.view
 import org.springframework.web.bind.annotation.{RequestMapping, RequestMethod, RestController}
 
 @RestController
-@RequestMapping(Array("/api/sample"))
 class SampleController {
-  @RequestMapping(method = Array(RequestMethod.GET))
+
+  @RequestMapping(value = Array("/api/sample"), method = Array(RequestMethod.GET))
   def data = "hoge"
+
+
+  @RequestMapping(value = Array("/api/redisgetsample"), method = Array(RequestMethod.GET))
+  def redisGetSample(): String = "Redisのテストに使う予定の建設予定地。"
+
 }
