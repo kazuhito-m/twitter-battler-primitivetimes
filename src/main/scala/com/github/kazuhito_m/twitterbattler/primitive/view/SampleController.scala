f64bfdc58883df20cf772470c209408cc36e6675
kazuhito-m <kazuhito.sumpic@gmail.com>
2016-11-16 18:07:32 +0900

    リファクタリング。RequestMappingアノテーションの多段化、URL整理。

--- src/main/scala/com/github/kazuhito_m/twitterbattler/primitive/view/SampleController.scala
+++ src/main/scala/com/github/kazuhito_m/twitterbattler/primitive/view/SampleController.scala
@@ -3,13 +3,14 @@ package com.github.kazuhito_m.twitterbattler.primitive.view
 import org.springframework.web.bind.annotation.{RequestMapping, RequestMethod, RestController}
 
 @RestController
+@RequestMapping(Array("/sample"))
 class SampleController {
 
-  @RequestMapping(value = Array("/api/sample"), method = Array(RequestMethod.GET))
+  @RequestMapping(value = Array("test"), method = Array(RequestMethod.GET))
   def data = "hoge"
 
 
-  @RequestMapping(value = Array("/api/redisgetsample"), method = Array(RequestMethod.GET))
+  @RequestMapping(value = Array("redisgetsample"), method = Array(RequestMethod.GET))
   def redisGetSample(): String = "Redisのテストに使う予定の建設予定地。"
 
 }
