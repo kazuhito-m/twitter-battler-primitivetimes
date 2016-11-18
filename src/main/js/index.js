e2b39a8745068e7df3fee754946c21486f87c4de
kazuhito-m <kazuhito.sumpic@gmail.com>
2016-11-18 18:43:06 +0900

    ソース修正。JSで「テストが通らなかったところ」を変更。#11

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -6,4 +6,8 @@ function getDefaultConditionValue() {
     const condition = new Condition();
     condition.productName = 'twitter-battler-primitivetimes';
     return condition.productName;
-};
+}
+
+// ESLintが「未使用だ！」怒るので…しゃーなしで書く。
+// (どうしてもかいくぐれないなら .eslintrc での対応を考える)
+getDefaultConditionValue();
--- src/test/js/condition_test.js
+++ src/test/js/condition_test.js
@@ -1,11 +1,11 @@
 'use strict';
 
 const assert = require('power-assert');
-const Condition = require('../main/condition');
+const Condition = require('../../main/js/condition');
 
 /**
  * 検索条件VOクラスのテスト。
- * 
+ *
  * 責務的には「VOなので少量の処理しかない」が、JSのプロパティ形式が不安なので、確認用。
  * 疑問を解決し経験を積むため、なるだけ冗長でもいいから丁寧にテスト書く。
  */
