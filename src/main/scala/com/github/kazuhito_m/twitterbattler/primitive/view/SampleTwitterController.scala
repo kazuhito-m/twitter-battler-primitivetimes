2db7139242d6c5e4cc355dbe428df236a42af642
kazuhito-m <kazuhito.sumpic@gmail.com>
2016-12-14 03:39:05 +0900

    実装。TwitterオブジェクトをDIして使ったサンプルContriollerを追加。

---
+++ src/main/scala/com/github/kazuhito_m/twitterbattler/primitive/view/SampleTwitterController.scala
@@ -0,0 +1,29 @@
+package com.github.kazuhito_m.twitterbattler.primitive.view
+
+import java.security.Principal
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.social.twitter.api.Twitter
+import org.springframework.web.bind.annotation.RequestMethod._
+import org.springframework.web.bind.annotation.{RequestMapping, RestController}
+
+@RestController
+@RequestMapping(Array("/api/twitter"))
+class SampleTwitterController {
+
+  @Autowired
+  val twitter: Twitter = null
+
+  @RequestMapping(value = Array("tl"), method = Array(GET, POST))
+  def timeline(user: Principal) = opeTwitter(user,
+    (name) => twitter.timelineOperations().getUserTimeline("@" + name)
+  )
+
+  @RequestMapping(value = Array("profile"), method = Array(GET, POST))
+  def profile(user: Principal) = opeTwitter(user,
+    (name) => twitter.userOperations().getUserProfile(name)
+  )
+
+  def opeTwitter(user: Principal, f: (String) => Object) = if (user == null) null else f(user.getName)
+
+}
