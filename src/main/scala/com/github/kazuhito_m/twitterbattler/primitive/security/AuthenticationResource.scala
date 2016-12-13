57fda645c6de785b2076c79809b816d574911061
kazuhito-m <kazuhito.sumpic@gmail.com>
2016-12-14 02:48:40 +0900

    リファクタリング。不要と思しきステートメントヲ削除。

--- src/main/scala/com/github/kazuhito_m/twitterbattler/primitive/security/AuthenticationResource.scala
+++ src/main/scala/com/github/kazuhito_m/twitterbattler/primitive/security/AuthenticationResource.scala
@@ -3,16 +3,11 @@ package com.github.kazuhito_m.twitterbattler.primitive.security
 import java.security.Principal
 import javax.servlet.http.HttpSession
 
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.web.bind.annotation.{RequestMapping, RequestMethod, RestController}
 
 @RestController
 @RequestMapping(Array("/api/session"))
 class AuthenticationResource {
-
-  @Autowired
-  val authenticationManager: AuthenticationManager = null
 
   @RequestMapping(method = Array(RequestMethod.GET))
   def session(user: Principal) = User(if (user == null) null else user.getName)
