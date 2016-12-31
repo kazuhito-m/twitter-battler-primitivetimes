f6758b5a60d608d3715b14afd158c95fc756a84a
kazuhito-m <kazuhito.sumpic@gmail.com>
2017-01-01 03:29:55 +0900

    微調整。こめんと修正

--- src/main/scala/com/github/kazuhito_m/twitterbattler/primitive/domain/repository/GameInformationRepository.scala
+++ src/main/scala/com/github/kazuhito_m/twitterbattler/primitive/domain/repository/GameInformationRepository.scala
@@ -31,7 +31,9 @@ class GameInformationRepository {
     */
   def deleteBattler(id: String): Unit = redisTemplate.delete(makeKey(id))
 
-  /** Battlerオブジェクトが存在するかを真偽値で返す。 */
+  /**
+    * Battlerオブジェクトが存在するかを真偽値で返す。
+    */
   def isExistsBattler(id: String) :Boolean = {getBattler(id) != null}
 
   /** RedisにBattlerオブジェクトを保存する文字列キーを作成する。 */
