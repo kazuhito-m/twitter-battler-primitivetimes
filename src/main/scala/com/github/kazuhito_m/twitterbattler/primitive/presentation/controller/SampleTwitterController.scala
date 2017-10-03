0dc0f33868ce3b79d01d34db153675080f6b294c
kazuhito-m <kazuhito.sumpic@gmail.com>
2017-10-04 03:14:29 +0900

    スパイク実装。RedisにJsonを保存する際のサンプルとそれをAPI的に叩くところを実験的実装。

--- src/main/scala/com/github/kazuhito_m/twitterbattler/primitive/presentation/controller/SampleTwitterController.scala
+++ src/main/scala/com/github/kazuhito_m/twitterbattler/primitive/presentation/controller/SampleTwitterController.scala
@@ -2,13 +2,16 @@ package com.github.kazuhito_m.twitterbattler.primitive.presentation.controller
 
 import java.security.Principal
 
+import com.github.kazuhito_m.twitterbattler.primitive.domain.model.battler.Battler
+import com.github.kazuhito_m.twitterbattler.primitive.domain.model.sample.Batlers
+import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.social.twitter.api.Twitter
 import org.springframework.web.bind.annotation.RequestMethod._
 import org.springframework.web.bind.annotation.{RequestMapping, RestController}
 
 @RestController
 @RequestMapping(Array("/api/twitter"))
-class SampleTwitterController(twitter: Twitter) {
+class SampleTwitterController(twitter: Twitter, redisTemplate: RedisTemplate[String, Object]) {
 
   @RequestMapping(value = Array("tl"), method = Array(GET, POST))
   def timeline(user: Principal) = opeTwitter(user,
@@ -21,5 +24,20 @@ class SampleTwitterController(twitter: Twitter) {
   def profile(user: Principal) = opeTwitter(user,
     (name) => twitter.userOperations().getUserProfile(name)
   )
+
+  @RequestMapping(value = Array("jsonTest"), method = Array(GET))
+  def jsonTest(): Unit = {
+    val one: Battler = new Battler
+    one.id = 1
+    one.twitterId = "testone"
+    val two: Battler = new Battler
+    two.id = 2
+    two.twitterId = "testtwo"
+
+    //    val battlers: Batlers = new Batlers(one, two)
+    val battlers: Batlers = new Batlers(List(one, two))
+    redisTemplate.opsForValue.set("test:abcd", battlers)
+
+  }
 
 }
