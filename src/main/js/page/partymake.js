1b4a8113999345e62984b2d21d26083ff32bc367
kazuhito-m <kazuhito.sumpic@gmail.com>
2017-10-13 02:00:39 +0900

    実装。パーティを表示するViewを実装。テスト未。

--- src/main/js/page/partymake.js
+++ src/main/js/page/partymake.js
@@ -38,6 +38,42 @@ class PartyMakePage {
         // イベント定義。
         html.addClickEventById('startBattle', (e) => this.startBattle(e, html, server));
 
+        // 初期表示。
+
+        // 味方パーティ&敵パーティの情報を取得。
+        const battle = server.getJson('api/battle/getBattle');
+
+        // 初期表示系
+        viewParties(battle, html);
+    }
+
+    viewParties(battle, html) {
+        alert(battle);
+        viewParty(battle.viewParty, 1, html);
+        viewParty(battle.enemyParty, 2, html);
+    }
+
+    viewParty(party, groupKey, html) {
+        let index = 1;
+        viewBattler(party.owner, groupKey, index, html);
+        party.members.forEach((member, index, ar) => {
+            viewBattler(member, groupKey, ++index, html);
+        });
+    }
+
+    viewBattler(battler, groupKey, index, html) {
+        const suffix = '_' + groupKey + '_' + index;
+        html.setImageSrcById("playerImage" + suffix, battler.imageUrl);
+
+        html.setTextById("playerId" + suffix, battler.twitterId);
+        html.setTextById("playerName" + suffix, battler.screenName);
+
+        html.setTextById("playerLv" + suffix, battler.level);
+        html.setTextById("playerAttackPoint" + suffix, battler.attackPoint);
+        html.setTextById("playerDefensePoint" + suffix, battler.defensePoint);
+        html.setTextById("playerSpeedPoint" + suffix, battler.speedPoint);
+        html.setTextById("playerMaxHitPoint" + suffix, battler.maxHitPoint);
+        html.setTextById("playerMaxSpecialPoint" + suffix, battler.maxSpecialPoint);
     }
 
 }
