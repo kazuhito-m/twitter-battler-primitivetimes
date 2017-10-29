9ff34f76e56407606d1c0a8de748676549dd1532
kazuhito-m <kazuhito.sumpic@gmail.com>
2017-10-30 04:13:51 +0900

    リファクタリング。不要な実装を削除。

--- src/main/scala/com/github/kazuhito_m/twitterbattler/primitive/domain/model/battle/command/Commands.scala
+++ src/main/scala/com/github/kazuhito_m/twitterbattler/primitive/domain/model/battle/command/Commands.scala
@@ -1,7 +1,4 @@
 package com.github.kazuhito_m.twitterbattler.primitive.domain.model.battle.command
 
 class Commands(val partyActivity: PartyActivity) {
-
-  // default constructor for Controller
-  def this() = this(PartyActivity.Fight)
 }
