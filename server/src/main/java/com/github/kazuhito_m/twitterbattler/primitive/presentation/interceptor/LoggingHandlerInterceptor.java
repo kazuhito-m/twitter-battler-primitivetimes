a952ab7cf2c67d244ca502060354dd884c5bd9aa
kazuhito_m <kazuhito.sumpic@gmail.com>
2020-01-26 02:23:30 +0900

    implement: HandlerMethodから「必要な値が出せるか」のため、ログに出力してみる。

--- server/src/main/java/com/github/kazuhito_m/twitterbattler/primitive/presentation/interceptor/LoggingHandlerInterceptor.java
+++ server/src/main/java/com/github/kazuhito_m/twitterbattler/primitive/presentation/interceptor/LoggingHandlerInterceptor.java
@@ -1,9 +1,12 @@
 package com.github.kazuhito_m.twitterbattler.primitive.presentation.interceptor;
 
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
@@ -25,6 +28,13 @@ public class LoggingHandlerInterceptor extends HandlerInterceptorAdapter {
 
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
         LOGGER.info("afterCompletion");
+        if (!(handler instanceof HandlerMethod)) return;
+        HandlerMethod method = (HandlerMethod) handler;
+        String log = ToStringBuilder.reflectionToString(method, ToStringStyle.MULTI_LINE_STYLE);
+        LOGGER.info("HandlerMethodの中身↓");
+        LOGGER.info(log);
+        LOGGER.info("response.getStatus():{}", response.getStatus());
+        LOGGER.info("getMethod:{}", method.getMethod());
     }
 
     public void afterConcurrentHandlingStarted(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
